bc40ac200335829ea5d6af563b5bda8aa4039f29
Justin Ledoux <justin.ledoux@quickseries.com>
2019-10-14 19:47:30 -0400

    Updated website dependencies and content.

--- config.js
+++ config.js
@@ -1,53 +1,51 @@
-var version = require( './package.json' ).version
-
 module.exports = {
-	local: {
-		baseURL: 								'http://localhost:' + process.env.PORT + '/',
-		staticResourcesBaseURL: '/',
-		jQuery: 								'/js-dev/lib/jquery-3.0.0.min.js',
-		staticResourceCache: 		{ maxage: 0, etag: false },
-		mongodb: {
-			options: {
-				server: {
-					socketOptions: { keepAlive: 1 }
-				}
-			},
-			connectionString: 'mongodb://localhost:27017/justinweb-local'
-		},
-		s3: {
-			accessKeyId: 'Your access key',
-			secretAccessKey: 'Your secret access key',
-			region: 'us-west-2'
-		}
-	},
+  local: {
+    baseURL: `http://localhost:${process.env.PORT}/`,
+    staticResourcesBaseURL: '/',
+    jQuery: '/js-dev/lib/jquery-3.0.0.min.js',
+    staticResourceCache: { maxage: 0, etag: false },
+    mongodb: {
+      options: {
+        server: {
+          socketOptions: { keepAlive: 1 },
+        },
+      },
+      connectionString: 'mongodb://localhost:27017/justinweb-local',
+    },
+    s3: {
+      accessKeyId: 'Your access key',
+      secretAccessKey: 'Your secret access key',
+      region: 'us-west-2',
+    },
+  },
 
-	staging: {
-		baseURL: 								'http://justinweb.heroku.com/',
-		staticResourcesBaseURL: '/',
-		jQuery: 								'/js-dev/lib/jquery-3.0.0.min.js',
-		staticResourceCache: 		{ maxage: 0, etag: false },
-		mongodb: {
-			options: {
-				server: {
-					socketOptions: { keepAlive: 1 }
-				}
-			},
-			connectionString: process.env.MONGOLAB_URI
-		}
-	},
+  staging: {
+    baseURL: 'http://justinweb.heroku.com/',
+    staticResourcesBaseURL: '/',
+    jQuery: '/js-dev/lib/jquery-3.0.0.min.js',
+    staticResourceCache: { maxage: 0, etag: false },
+    mongodb: {
+      options: {
+        server: {
+          socketOptions: { keepAlive: 1 },
+        },
+      },
+      connectionString: process.env.MONGOLAB_URI,
+    },
+  },
 
-	production: {
-		baseURL: 								'http://www.justinweb.pro/',
-		staticResourcesBaseURL: '/',
-		jQuery: 								'/js-dev/lib/jquery-3.0.0.min.js',
-		staticResourceCache: 		{ maxage: 0, etag: false },
-		mongodb: {
-			options: {
-				server: {
-					socketOptions: { keepAlive: 1 }
-				}
-			},
-			connectionString: process.env.MONGOLAB_URI
-		}
-	}
-}
+  production: {
+    baseURL: 'http://www.justinweb.pro/',
+    staticResourcesBaseURL: '/',
+    jQuery: '/js-dev/lib/jquery-3.0.0.min.js',
+    staticResourceCache: { maxage: 0, etag: false },
+    mongodb: {
+      options: {
+        server: {
+          socketOptions: { keepAlive: 1 },
+        },
+      },
+      connectionString: process.env.MONGOLAB_URI,
+    },
+  },
+};
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-module.exports = function ( app ) {
-	app.listen( process.env.PORT || 3000, function () {
-		console.log(
-			'App listening at http://%s:%s',
-			process.env.NODE_ENV,
-			process.env.PORT
-		)
-	})
-}
+module.exports = (app) => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(
+      'App listening at http://%s:%s',
+      process.env.NODE_ENV,
+      process.env.PORT,
+    );
+  });
+};
